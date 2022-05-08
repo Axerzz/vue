b11d0755e1252a689743486bbdcaa219d799d22a
Axerzz <90019502+Axerzz@users.noreply.github.com>
2022-05-08 20:43:57 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath :'./ ',
   lintOnSave: false,
   transpileDependencies: true,
   configureWebpack: { externals: {
     'AMap': 'AMap'
   }, },
   devServer: {
-    port: 9876,
+    port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:9090',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true, //是否跨域
         pathRewrite: {
           '^/api': ''
